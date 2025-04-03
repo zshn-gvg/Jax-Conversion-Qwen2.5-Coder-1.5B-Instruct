0897c40977cf13a8f45d7d0b3fa9624c19dad576
Brecht Francois <brecht.francois@showpad.com>
2025-04-03 20:07:02 +0200

    torch model name correction

--- model_torch.py
+++ model_torch.py
@@ -841,7 +841,7 @@ class Qwen2ForCausalLM(nn.Module):
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
+model_name = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
